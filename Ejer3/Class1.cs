e668658c816054441aba1ad1ee851055246d9a7f
gastonsebas01 <gaseri001@gmail.com>
2025-04-08 22:37:11 -0300

    Add files via upload

---
+++ Ejer3/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejer3
+{
+    public class Animal
+    {
+        public string especie;
+        public string genero;
+        public int edad;
+        public string tamanio;
+        public string sonido;
+
+        public void alimentar()
+        {
+            Console.WriteLine("El "+especie+" esta comiendo.");
+        }
+        public void reproducir()
+        {
+            Console.WriteLine("El "+especie+" se esta reproduciendo.");
+        }
+        public void sonidos()
+        {
+            Console.WriteLine("El "+especie+ " esta "+sonido);
+        }
+        public void mostrarAnimal()
+        {
+            string especie = this.especie;
+            string genero = this.genero;
+            string tamanio = this.tamanio;
+            string sonido = this.sonido;
+            int edad = this.edad;
+
+            Console.WriteLine(especie + '\n' +genero + '\n' + tamanio + '\n' + edad+" años");
+        }
+    }
+}
---
+++ Ejer3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Ejer3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal gato=new Animal();
+            gato.especie = "felino";
+            gato.edad = 3;
+            gato.tamanio = "mediano";
+            gato.genero = "hembra";
+            gato.sonido = "maullando";
+            gato.mostrarAnimal();
+            gato.alimentar();
+
+            Animal raton=new Animal();
+            raton.especie = "roedor";
+            raton.edad = 1;
+            raton.tamanio = "pequenio";
+            raton.genero = "macho";
+            raton.sonido = "chillando";
+            raton.mostrarAnimal();
+            raton.reproducir();
+
+            Animal caballo=new Animal();
+            caballo.especie = "equino";
+            caballo.edad = 10;
+            caballo.tamanio = "grande";
+            caballo.genero = "macho";
+            caballo.sonido = "relinchando";
+            caballo.mostrarAnimal();
+            caballo.sonidos();
+
+        }
+    }
+}
