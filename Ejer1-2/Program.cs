40fc2ade5b14565c5ea2cd130ca7ea21d332f976
gastonsebas <gastonsebas@nsmx1>
2025-04-08 21:39:28 -0300

    Agregar archivos de proyecto.

---
+++ Ejer1-2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Ejer1_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Autobus autobus1=new Autobus();
+            autobus1.color = "verde";
+            autobus1.anio = 2020;
+            autobus1.cantAsientos = 33;
+            autobus1.cantPuertas = 3;
+            autobus1.marca = "Volkswagen";
+            autobus1.modelo = "EOD";
+            autobus1.nroSerie = 12385;
+            autobus1.cantPasajeros = 35;
+            autobus1.mostrarAutobus();
+            autobus1.cargarPasajeros();
+            autobus1.arrancar();
+            autobus1.detener();
+            autobus1.descargaPasajeros();
+
+
+
+            Vehiculo moto1=new Vehiculo();
+            moto1.marca = "Honda";
+            moto1.modelo = "NXM";
+            moto1.nroSerie = 512;
+            moto1.color = "rojo";
+            moto1.anio = 2019;
+            moto1.mostrarVehiculo();
+            moto1.arrancar();
+
+            Vehiculo camion = new Vehiculo();
+            camion.marca = "Scania";
+            camion.modelo = "RGC";
+            camion.nroSerie = 856;
+            camion.color = "blanco";
+            camion.anio = 2018;
+            camion.mostrarVehiculo();
+            camion.detener();
+
+            Vehiculo auto=new Vehiculo();
+            auto.marca = "Ford";
+            auto.modelo = "TYN";
+            auto.nroSerie = 468;
+            auto.color = "azul";
+            auto.anio = 2021;
+            auto.mostrarVehiculo();
+            auto.arrancar();
+        }
+    }
+}
---
+++ Ejer1-2/Vehiculo.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejer1_2
+{
+    public class Vehiculo
+    {
+        public string modelo;
+        public string marca;
+        public int nroSerie;
+        public string color;
+        public int anio;
+
+        public void arrancar()
+        {
+            Console.WriteLine("El vehiculo esta arrancando.");
+        }
+        public void frenar()
+        {
+            Console.WriteLine("El vehiculo esta frenando.");
+        }
+        public void acelerar()
+        {
+            Console.WriteLine("El vehiculo esta acelerando.");
+        }
+        public void detener()
+        {
+            Console.WriteLine("El vehiculo se esta deteniendo.");
+        }
+        public void mostrarVehiculo()
+        {
+           string modelo=this.modelo;
+           string marca = this.marca;
+           int nroSerie=this.nroSerie;
+           string color=this.color;
+           int anio=this.anio;
+
+            Console.WriteLine(marca+'\n'+ modelo+ '\n'+ nroSerie+ '\n'+ color+ '\n'+ anio);
+        }
+
+    }
+    public class Autobus : Vehiculo
+    {
+        public int cantPasajeros;
+        public int cantAsientos;
+        public int cantPuertas;
+
+        public void cargarPasajeros()
+        {
+            Console.WriteLine("El autobus esta cargando pasajeros.");
+        }
+        public void descargaPasajeros()
+        {
+            Console.WriteLine("El autobus esta descargando pasajeros.");
+        }
+        public void mostrarAutobus() {
+            Console.WriteLine("marca: " + marca);
+            Console.WriteLine("modelo: "+modelo);
+            Console.WriteLine("color: " + color);
+            Console.WriteLine("nro de serie: "+nroSerie);
+            Console.WriteLine("año: "+anio);
+            Console.WriteLine("pasajeros: "+cantPasajeros);
+            Console.WriteLine("asientos: "+cantAsientos);
+            Console.WriteLine("puertas: "+cantPuertas);
+        }
+        
+    }
+}
